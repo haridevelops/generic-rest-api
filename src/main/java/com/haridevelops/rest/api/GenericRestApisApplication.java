7e70e45970a04059602a0c29f8400f6ba435cc15
haridevelops <m.harisudhan95@gmail.com>
2019-04-02 22:14:16 +0530

    initial commit

---
+++ src/main/java/com/haridevelops/rest/api/GenericRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.haridevelops.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericRestApisApplication.class, args);
+	}
+
+}
